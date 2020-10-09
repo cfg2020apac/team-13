2139292555065ec7f10fbc9461995db69aac84fb
Jing Ying <e0323887@u.nus.edu>
2020-10-10 02:58:43 +0800

    Add headers

--- Backend/index.js
+++ Backend/index.js
@@ -14,10 +14,21 @@ const PORT = process.env.PORT || 4000;
 // Middleware
 app.use(bodyParser.json());
 
+//Set headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Request-With, Content-Type, Accept"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
+
 app.get("/HAHA", (req, res) => {  // To extract data
   res.json({ message: "Hello Friends!" });
 });
-app.use("/web", site); 
+app.use("/web", site);
 app.use("/data", mockdata); // Data Analytics
 
 app.listen(PORT, (req, res) => {
